aeafb99ceacdf3778b70444d966d3f0e9730b0ff
ara4dev <aracom101@gmail.com>
2020-09-26 18:23:19 +0100

    init commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDnjZpVh5I1lBN6cPAl1SSSeHoh8yK5Vao",
+    authDomain: "whatsapp-mern-1629e.firebaseapp.com",
+    databaseURL: "https://whatsapp-mern-1629e.firebaseio.com",
+    projectId: "whatsapp-mern-1629e",
+    storageBucket: "whatsapp-mern-1629e.appspot.com",
+    messagingSenderId: "802444292812",
+    appId: "1:802444292812:web:b5a0bcfeca367e4913c869"
+  };
